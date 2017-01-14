70f51f88983aece9290fcd97585f74ff631ad734
john ryan zelling <jryanzelling@gmail.com>
2017-01-14 03:10:44 -0800

    Initial needs data generators

---
+++ README.md
@@ -0,0 +1,11 @@
+This is a production app for searching and finding patterns in data. 
+
+It was developed with sensitive data so having to rewrite the application for general use. 
+
+Project requires four data files loaded into memory. 
+
+Working on a function to generate fake data. 
+
+License MIT. 
+
+Written by John Ryan zelling. Contributors welcome. 
---
+++ searcher-master.R
@@ -0,0 +1,138 @@
+library("shiny")
+library("shinydashboard")
+library("DT")
+
+# This app requires your four data tables to be loaded from DataHelperFile.R
+# Subsequent forks of this app can use less or more by amending code in app.r
+#load("table1.dt.rda")
+#load("table2.dt.rda")
+#load("table3.rda")
+#load("table4.rda")
+
+ui <- dashboardPage(
+  
+  # Dashboard Header
+  dashboardHeader(title = "hids database browser"),
+  
+  # Dashboard Sidebar 
+  dashboardSidebar(disable = TRUE),
+  
+  # Dashboard Body
+  dashboardBody(
+    
+    # Sidebar Layout includes a single side bar and a main panel
+    sidebarLayout(
+      
+      # Side Panel Main Page Layout Tab
+      sidebarPanel(
+        
+        # Show this panel only if dataset is cve_main
+        conditionalPanel('input.dataset === "cve_main"', helpText("cve_main"),
+                         checkboxGroupInput(
+                           'show_vars1', 
+                           'Columns in cve_main to show:', 
+                           names(cve.main.dt), 
+                           selected = names(cve.main.dt)
+                         )
+        ),
+        
+        # Show this panel only if dataset is cve_main_servers
+        conditionalPanel('input.dataset === "cve_main_servers"', helpText("cve_main_servers"),
+                         checkboxGroupInput(
+                           'show_vars2', 
+                           'Columns in cve_main_servers to show:', 
+                           names(cve.main.servers.dt), 
+                           selected = names(cve.main.servers.dt))
+        ),
+        
+        # Show this panel only if dataset is mir_main
+        conditionalPanel('input.dataset === "mir_main"', helpText('mir_main'),
+                         checkboxGroupInput(
+                           'show_vars3', 
+                           'Columns in mir_main to show:', 
+                           names(mir.main.dt), 
+                           selected = names(mir.main.dt))
+        ),
+        
+        # Show this panel only if the dataset is mir_mbmrlogs
+        conditionalPanel('input.dataset === "mir_mbmrlogs"', helpText('mir_mbmrlogs'),
+                         checkboxGroupInput(
+                           'show_vars4', 
+                           'Columns in mir_mbmrlogs to show:', 
+                           names(mir.mbmrlogs.dt), 
+                           selected = names(mir.mbmrlogs.dt)
+                         )
+        ),
+        
+        # Sets the width of the sidebarPanel()
+        width = 2),
+      
+      # Main panel of sidebar layout
+      mainPanel(
+        navbarPage(title = "Data browser",
+                   id="dataset",
+                   theme = "bootstrap.css",
+                   position = "static-top",
+                   tabPanel('cve_main', DT::dataTableOutput('cve_main_dt')),
+                   tabPanel('cve_main_servers', DT::dataTableOutput('cve_main_servers_dt')),
+                   tabPanel('mir_main', DT::dataTableOutput('mir_main_dt')),
+                   tabPanel('mir_mbmrlogs', DT::dataTableOutput('mir_mbmrlogs_dt'))
+        )
+      )
+    )
+  )
+)
+
+server <- function(input, output) {
+  
+  #Global datatable Options
+  options(DT.options = list(
+    pageLength = 10, 
+    language = list(search = 'Filter:'), 
+    autoWidth = TRUE, 
+    searchHighlight = TRUE,
+    orderClasses = TRUE,
+    
+    # columnDefs = list(list(
+    #   targets = c(4, 5, 6, 7, 8, 9, 10),
+    #   render = JS(
+    #     "function(data, type, row, meta) {",
+    #     "return type === 'display' && data.length > 30 ?",
+    #     "'<span title=\"' + data + '\">' + data.substr(0, 6) + '...</span>' : data;",
+    #     "}"))), 
+    
+    initComplete = JS(
+      "function(settings, json) {",
+      "$(this.api().table().header()).css({'background-color': '#000', 'color': '#fff'});",
+      "}"),
+    
+    lengthMenu = list(c(5, 10, 30, 50,-1), c("5", "10", "30", "50", "All"))
+    
+  ))
+  
+  output$cve_main_dt <- DT::renderDataTable({
+    DT::datatable(filter = "bottom",
+      cve.main.dt[, input$show_vars1, drop = FALSE], 
+      options = list())}
+  )
+  
+  output$cve_main_servers_dt <- DT::renderDataTable({
+    DT::datatable(filter = "bottom",
+      cve.main.servers.dt[, input$show_vars2, drop = FALSE], 
+      options = list())}
+  )
+  
+  output$mir_main_dt <- DT::renderDataTable({
+    DT::datatable(filter = "bottom",
+      mir.main.dt[, input$show_vars3, drop = FALSE], 
+      options = list())}
+  )
+  
+  output$mir_mbmrlogs_dt <- DT::renderDataTable({
+    DT::datatable(filter = "bottom",
+      mir.mbmrlogs.dt[, input$show_vars4, drop = FALSE], 
+      options = list())}
+  )
+}
+
+shinyApp(ui, server)
